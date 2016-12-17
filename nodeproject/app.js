5029e4610db5e28f6a90f5f34bff6af1fe3ab67d
John Villamar <john.villamar@lc.cuny.edu>
2016-12-17 21:36:57 +0000

    Refactor and included bower and gulp

--- nodeproject/app.js
+++ nodeproject/app.js
@@ -1,2 +1,20 @@
-var aloha = 'Hello World';
-console.log(aloha);
+var express = require("express");
+var app = express();
+
+var port = process.env.PORT; // Global variable from c9
+
+app.use(express.static('public'));
+app.use(express.static('src/views'));
+app.use(express.static('bower_components'));
+
+app.get('/', function(req, res){
+    res.send('Hello world!');
+});
+
+app.get('/routing', function(req, res){
+    res.send('Hello routing!');
+});
+
+app.listen(port, function(err) {
+    console.log('The server is running on port: ' + port);
+});
